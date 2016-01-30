c07c3d83dedc370fcb027c953b27ab3fa2d4ac0c
iBeech <tombeech@me.com>
2016-01-30 22:20:53 +0000

    Update PlexHomeTheatre.groovy

--- PlexManager/PlexHomeTheatre.groovy
+++ PlexManager/PlexHomeTheatre.groovy
@@ -18,6 +18,11 @@ metadata {
 	definition (name: "Plex Home Theatre", namespace: "ibeech", author: "ibeech") {
     	capability "Switch"
 		capability "Music Player"
+        
+        command "scanNewClients"
+        command "setPlaybackIcon", ["string"]
+        command "setPlaybackTitle", ["string"]
+        command "setVolumeLevel", ["number"]        
 	}
 
 	simulator {
@@ -32,13 +37,41 @@ metadata {
 			state "paused", label:'Paused', action:"music Player.play", icon:"st.Electronics.electronics16", nextState:"playing", backgroundColor:"#FFA500"
         }
         
+        standardTile("next", "device.status", width: 1, height: 1, decoration: "flat") {
+			state "next", label:'', action:"music Player.nextTrack", icon:"st.sonos.next-btn", backgroundColor:"#ffffff"
+		}
+        
+        standardTile("previous", "device.status", width: 1, height: 1, decoration: "flat") {
+			state "previous", label:'', action:"music Player.previousTrack", icon:"st.sonos.previous-btn", backgroundColor:"#ffffff"
+		}	
+        
+        standardTile("scanNewClients", "device.status", width: 2, height: 1, decoration: "flat") {
+			state "default", label:'', action:"scanNewClients", icon:"state.icon", backgroundColor:"#ffffff"
+			state "grouped", label:'', action:"scanNewClients", icon:"state.icon", backgroundColor:"#ffffff"
+		}
+        
+        standardTile("fillerTile", "device.status", width: 1, height: 1, decoration: "flat") {
+			state "default", label:'', action:"", icon:"", backgroundColor:"#ffffff"
+			state "grouped", label:'', action:"", icon:"", backgroundColor:"#ffffff"
+		}
+        
         standardTile("stop", "device.status", width: 1, height: 1, decoration: "flat") {
 			state "default", label:'', action:"music Player.stop", icon:"st.sonos.stop-btn", backgroundColor:"#ffffff"
 			state "grouped", label:'', action:"music Player.stop", icon:"st.sonos.stop-btn", backgroundColor:"#ffffff"
 		}
+        
+        	
+        valueTile("currentSong", "device.trackDescription", inactiveLabel: true, height:1, width:3, decoration: "flat") {
+            state "default", label:'${currentValue}', backgroundColor:"#ffffff"
+        }
+    
+        controlTile("levelSliderControl", "device.level", "slider", height: 1, width: 3, inactiveLabel: false) {
+            state "level", action:"setVolumeLevel", backgroundColor:"#ffffff"
+        }
+        
 
 		main "main"
-		details (["main", "stop"])
+		details (["currentSong", "previous", "main", "next", "fillerTile", "stop", "fillerTile", "levelSliderControl","fillerTile", "scanNewClients"])
 	}
 }
 
@@ -49,6 +82,7 @@ def parse(String description) {
 
 def play() {
 	log.debug "Executing 'on'"	     
+    
     
     sendEvent(name: "switch", value: device.deviceNetworkId + ".play");    
     sendEvent(name: "switch", value: "on");    
@@ -69,10 +103,44 @@ def stop() {
 	sendEvent(name: "switch", value: device.deviceNetworkId + ".stop");     
     sendEvent(name: "switch", value: "off");
     sendEvent(name: "status", value: "stopped");
+    setPlaybackTitle("Stopped");
+}
+
+def previousTrack() {
+	log.debug "Executing 'previous': "
+    
+    setPlaybackTitle("Skipping previous");
+    sendCommand("previous");    
+}
+
+def nextTrack() {
+	log.debug "Executing 'next'"
+
+	setPlaybackTitle("Skipping next");
+	sendCommand("next");
+}
+
+def scanNewClients() {
+	log.debug "Executing 'scanNewClients'"        
+    sendCommand("scanNewClients");
+}
+
+def setVolumeLevel(level) {
+	log.debug "Executing 'setVolumeLevel(" + level + ")'"
+    sendEvent(name: "level", value: level);
+    sendCommand("setVolume." + level);
+}
+
+def sendCommand(command) {
+	
+    def lastState = device.currentState('switch').getValue();
+    sendEvent(name: "switch", value: device.deviceNetworkId + "." + command);
+    sendEvent(name: "switch", value: lastState);
 }
 
 def setPlaybackState(state) {
 
+	log.debug "Executing 'setPlaybackState'"
     switch(state) {
         case "stopped":
         sendEvent(name: "switch", value: "off");
@@ -88,4 +156,21 @@ def setPlaybackState(state) {
         sendEvent(name: "switch", value: "off");
         sendEvent(name: "status", value: "paused");
     }
+}
+
+def setPlaybackTitle(text) {
+	log.debug "Executing 'setPlaybackTitle'"
+    
+    sendEvent(name: "trackDescription", value: text)
+}
+
+def setPlaybackIcon(iconUrl) {
+	log.debug "Executing 'setPlaybackIcon'"
+    
+    state.icon = iconUrl;
+    
+    //sendEvent(name: "scanNewClients", icon: iconUrl)
+    //sendEvent(name: "scanNewClients", icon: iconUrl)
+    
+    log.debug "Icon set to " + state.icon
 }
