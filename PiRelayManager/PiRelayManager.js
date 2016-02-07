9be8aadf59ea3abdc54c3a1286a3a0490acb366e
iBeech <tombeech@me.com>
2016-02-07 21:41:29 +0000

    Update PiRelayManager.js

--- PiRelayManager/PiRelayManager.js
+++ PiRelayManager/PiRelayManager.js
@@ -19,148 +19,278 @@ definition(
     author: "ibeech",
     description: "Add each Pi Relay as an individual thing.",
     category: "Safety & Security",
-    iconUrl: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience.png",
-    iconX2Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png",
-    iconX3Url: "https://s3.amazonaws.com/smartapp-icons/Convenience/Cat-Convenience@2x.png")
+    iconUrl: "http://download.easyicon.net/png/1179528/64/",
+    iconX2Url: "http://download.easyicon.net/png/1179528/128/",
+    iconX3Url: "http://download.easyicon.net/png/1179528/128/")
 
 
 preferences {
 
   section("Raspberry Pi Setup"){
   	input "piIP", "text", "title": "Raspberry Pi IP", multiple: false, required: true
-    	input "piPort", "text", "title": "Raspberry Pi Port", multiple: false, required: true
-    	input "theHub", "hub", title: "On which hub?", multiple: false, required: true
+    input "piPort", "text", "title": "Raspberry Pi Port", multiple: false, required: true
+    input "theHub", "hub", title: "On which hub?", multiple: false, required: true
   }
   
-    section("GPIO Setup") {
-	input "gpioName2", "text", title: "Relay 2 Name", required:false
-	input "gpioName3", "text", title: "Relay 3 Name", required:false
-	input "gpioName4", "text", title: "Relay 4 Name", required:false
-	input "gpioName7", "text", title: "Relay 7 Name", required:false
-	input "gpioName8", "text", title: "Relay 8 Name", required:false
-	input "gpioName9", "text", title: "Relay 9 Name", required:false
-	input "gpioName10", "text", title: "Relay 10 Name", required:false
-    	input "gpioName11", "text", title: "Relay 11 Name", required:false
-    	input "gpioName17", "text", title: "Relay 17 Name", required:false
-    	input "gpioName18", "text", title: "Relay 18 Name", required:false
-    	input "gpioName22", "text", title: "Relay 22 Name", required:false
-    	input "gpioName23", "text", title: "Relay 23 Name", required:false
-    	input "gpioName24", "text", title: "Relay 24 Name", required:false
-    	input "gpioName25", "text", title: "Relay 25 Name", required:false
-    	input "gpioName27", "text", title: "Relay 27 Name", required:false
-  }
+    section("Device 1") {    
+		input "deviceName1", "text", title: "Device Name", required:false	        
+        input "deviceType1", "enum", title: "Device Type", required: false, options: [                
+                "switch":"Relay Switch",
+                "temperatureSensor":"Temperature Sensor"]
+        input "deviceConfig1", "text", title: "GPIO# or Device Name", required: false
+    }
+    section("Device 2") {    
+		input "deviceName2", "text", title: "Device Name", required:false	        
+        input "deviceType2", "enum", title: "Device Type", required: false, options: [                
+                "switch":"Relay Switch",
+                "temperatureSensor":"Temperature Sensor"]
+        input "deviceConfig2", "text", title: "GPIO# or Device Name", required: false
+    }  
+    section("Device 3") {    
+		input "deviceName3", "text", title: "Device Name", required:false	        
+        input "deviceType3", "enum", title: "Device Type", required: false, options: [                
+                "switch":"Relay Switch",
+                "temperatureSensor":"Temperature Sensor"]
+        input "deviceConfig3", "text", title: "GPIO# or Device Name", required: false
+    }      
+    section("Device 4") {    
+		input "deviceName4", "text", title: "Device Name", required:false	        
+        input "deviceType4", "enum", title: "Device Type", required: false, options: [                
+                "switch":"Relay Switch",
+                "temperatureSensor":"Temperature Sensor"]
+        input "deviceConfig4", "text", title: "GPIO# or Device Name", required: false
+    }          
+    section("Device 5") {    
+		input "deviceName5", "text", title: "Device Name", required:false	        
+        input "deviceType5", "enum", title: "Device Type", required: false, options: [                
+                "switch":"Relay Switch",
+                "temperatureSensor":"Temperature Sensor"]
+        input "deviceConfig5", "text", title: "GPIO# or Device Name", required: false
+    }    
+    section("Device 6") {    
+		input "deviceName6", "text", title: "Device Name", required:false	        
+        input "deviceType6", "enum", title: "Device Type", required: false, options: [                
+                "switch":"Relay Switch",
+                "temperatureSensor":"Temperature Sensor"]
+        input "deviceConfig6", "text", title: "GPIO# or Device Name", required: false
+    }    
+    section("Device 7") {    
+		input "deviceName7", "text", title: "Device Name", required:false	        
+        input "deviceType7", "enum", title: "Device Type", required: false, options: [                
+                "switch":"Relay Switch",
+                "temperatureSensor":"Temperature Sensor"]
+        input "deviceConfig7", "text", title: "GPIO# or Device Name", required: false
+    }   
+    section("Device 8") {    
+		input "deviceName8", "text", title: "Device Name", required:false	        
+        input "deviceType8", "enum", title: "Device Type", required: false, options: [                
+                "switch":"Relay Switch",
+                "temperatureSensor":"Temperature Sensor"]
+        input "deviceConfig8", "text", title: "GPIO# or Device Name", required: false
+    }        
+    section("Device 9") {    
+		input "deviceName9", "text", title: "Device Name", required:false	        
+        input "deviceType9", "enum", title: "Device Type", required: false, options: [                
+                "switch":"Relay Switch",
+                "temperatureSensor":"Temperature Sensor"]
+        input "deviceConfig9", "text", title: "GPIO# or Device Name", required: false
+    }        
+    section("Device 10") {    
+		input "deviceName10", "text", title: "Device Name", required:false	        
+        input "deviceType10", "enum", title: "Device Type", required: false, options: [                
+                "switch":"Relay Switch",
+                "temperatureSensor":"Temperature Sensor"]
+        input "deviceConfig10", "text", title: "GPIO# or Device Name", required: false
+    }  
 }
 
 def installed() {
-	
-	log.debug "Installed with settings: ${settings}"
-	initialize()
+  log.debug "Installed with settings: ${settings}"
+
+  initialize()
 }
 
 def initialize() {
 
     subscribe(location, null, response, [filterEvents:false])    
    
-	setupVirtualRelay("2", gpioName2);
-	setupVirtualRelay("3", gpioName3);
- 	setupVirtualRelay("4", gpioName4);
-	setupVirtualRelay("7", gpioName7);
-	setupVirtualRelay("8", gpioName8);
-	setupVirtualRelay("9", gpioName9);
-	setupVirtualRelay("10", gpioName10);
-	setupVirtualRelay("11", gpioName11);
-	setupVirtualRelay("17", gpioName17);
-	setupVirtualRelay("18", gpioName18);
-	setupVirtualRelay("22", gpioName22);
-	setupVirtualRelay("23", gpioName23);
-	setupVirtualRelay("24", gpioName24);
-	setupVirtualRelay("25", gpioName25);
-	setupVirtualRelay("27", gpioName27);
+	setupVirtualRelay(deviceName1, deviceType1, deviceConfig1);
+    setupVirtualRelay(deviceName2, deviceType2, deviceConfig2);
+    setupVirtualRelay(deviceName3, deviceType3, deviceConfig3);
+    setupVirtualRelay(deviceName4, deviceType4, deviceConfig4);
+    setupVirtualRelay(deviceName5, deviceType5, deviceConfig5);
+    setupVirtualRelay(deviceName6, deviceType6, deviceConfig6);
+    setupVirtualRelay(deviceName7, deviceType7, deviceConfig7);
+    setupVirtualRelay(deviceName8, deviceType8, deviceConfig8);
+    setupVirtualRelay(deviceName9, deviceType9, deviceConfig9);
+    setupVirtualRelay(deviceName10, deviceType10, deviceConfig10);
 }
 
 def updated() {
 	log.debug "Updated with settings: ${settings}"
+    
+    updateGPIOState();
+    unsubscribe();
+    
+    updateVirtualRelay(deviceName1, deviceType1, deviceConfig1);
+    updateVirtualRelay(deviceName2, deviceType2, deviceConfig2);    
+    updateVirtualRelay(deviceName3, deviceType3, deviceConfig3);
+    updateVirtualRelay(deviceName4, deviceType4, deviceConfig4);
+    updateVirtualRelay(deviceName5, deviceType5, deviceConfig5);
+    updateVirtualRelay(deviceName6, deviceType6, deviceConfig6);
+    updateVirtualRelay(deviceName7, deviceType7, deviceConfig7);
+    updateVirtualRelay(deviceName8, deviceType8, deviceConfig8);
+    updateVirtualRelay(deviceName9, deviceType9, deviceConfig9);
+    updateVirtualRelay(deviceName10, deviceType10, deviceConfig10);
+    
+    subscribe(location, null, response, [filterEvents:false])
+}
+
+def updateVirtualRelay(deviceName, deviceType, deviceConfig) {
+    
+    // If user didn't fill this device out, skip it
+    if(!deviceName) return;
+    
+    def theDeviceNetworkId = "";
+    switch(deviceType) {
+    	case "switch":
+        	theDeviceNetworkId = getRelayID(deviceConfig);
+            break;
             
-    	unsubscribe();
-    	
-	updateVirtualRelay("2", gpioName2);
-	updateVirtualRelay("3", gpioName3);
- 	updateVirtualRelay("4", gpioName4);
-	updateVirtualRelay("7", gpioName7);
-	updateVirtualRelay("8", gpioName8);
-	updateVirtualRelay("9", gpioName9);
-	updateVirtualRelay("10", gpioName10);
-	updateVirtualRelay("11", gpioName11);
-	updateVirtualRelay("17", gpioName17);
-	updateVirtualRelay("18", gpioName18);
-	updateVirtualRelay("22", gpioName22);
-	updateVirtualRelay("23", gpioName23);
-	updateVirtualRelay("24", gpioName24);
-	updateVirtualRelay("25", gpioName25);
-	updateVirtualRelay("27", gpioName27);
+        case "temperatureSensor":
+        	theDeviceNetworkId = getTemperatureID(deviceConfig);
+            break;
+    }
     
-    	subscribe(location, null, response, [filterEvents:false])    
-}
-
-def updateVirtualRelay(deviceId, gpioName){
-
-	def children = getChildDevices()
-    	def theDeviceNetworkId = "piRelay." + settings.piIP;
+    log.trace "Searching for: $theDeviceNetworkId";
     
-  	def theSwitch = children.find{ d -> d.deviceNetworkId.startsWith(theDeviceNetworkId) }  
+  	def theDevice = getChildDevices().find{ d -> d.deviceNetworkId.startsWith(theDeviceNetworkId) }  
     
-    	if(theSwitch){ // The switch already exists
-    
-    		if(!gpioName){ // The user has not filled out data for this device
-    			log.debug "Found an existing device, but the user has chosen to delete this particular one"
-        		deleteChildDevice(theSwitch.deviceNetworkId);
-    		} else {
+    if(theDevice){ // The switch already exists
+    	log.debug "Found existing device which we will now update"   
+        theDevice.deviceNetworkId = theDeviceNetworkId + "." + deviceConfig
+        theDevice.label = deviceName
+        theDevice.name = deviceName
         
-    			log.debug "Found existing switch which we will now update"   
-        		theSwitch.deviceNetworkId = theDeviceNetworkId + "." + deviceId
-        		theSwitch.label = gpioName
-        		theSwitch.name = gpioName
-        		
-            		subscribe(theSwitch, "switch", switchChange)
-            		
-        		log.debug "Setting initial state of $gpioName to off"
-        		setDeviceState(deviceId, "off");
-	    		theSwitch.off();
-		}
-    	} else { // The switch does not exist
-    		if(gpioName){ // The user filled in data about this switch
-    			log.debug "This switch does not exist, creating a new one now"
-        		setupVirtualRelay(deviceId, gpioName);
-       		}
-    	}
+        if(deviceType == "switch") { // Actions specific for the relay device type
+            subscribe(theSwitch, "switch", switchChange)
+            log.debug "Setting initial state of $deviceName to off"
+            setDeviceState(deviceConfig, "off");
+            theDevice.off();
+        } else {
+        	updateTempratureSensor();
+        }
+        
+    } else { // The switch does not exist
+    	if(deviceName){ // The user filled in data about this switch
+    		log.debug "This device does not exist, creating a new one now"
+        	/*setupVirtualRelay(deviceId, gpioName);*/
+            setupVirtualRelay(deviceName, deviceType, deviceConfig);
+       	}
+    }
+
 }
+def setupVirtualRelay(deviceName, deviceType, deviceConfig) {
 
-def setupVirtualRelay(deviceId, gpioName){
-
-	if(gpioName){
-		log.debug "Create a Virtual Pi Relay named $gpioName"
-		def d = addChildDevice("ibeech", "Virtual Pi Relay", "piRelay." + settings.piIP + "." + deviceId, theHub.id, [label:gpioName, name:gpioName])
-		subscribe(d, "switch", switchChange)
-		    
-		log.debug "Setting initial state of $gpioName to off"
-		setDeviceState(deviceId, "off");
-		d.off();
-		log.debug "Virtual Pi Relay $gpioName created"
+	if(deviceName){
+        log.debug deviceName
+	    log.debug deviceType
+        log.debug deviceConfig
+        
+        switch(deviceType) {
+        	case "switch":
+            	log.trace "Found a relay switch called $deviceName on GPIO #$deviceConfig"
+				def d = addChildDevice("ibeech", "Virtual Pi Relay", getRelayID(deviceConfig), theHub.id, [label:deviceName, name:deviceName])
+	    		subscribe(d, "switch", switchChange)
+                
+	    		log.debug "Setting initial state of $gpioName to off"
+        		setDeviceState(deviceConfig, "off");
+	    		d.off();
+            	break;
+                    
+            case "temperatureSensor":
+			  	log.trace "Found a temperature sensor called $deviceName on $deviceConfig"
+                def d = addChildDevice("ibeech", "Virtual Pi Temperature", getTemperatureID(deviceConfig), theHub.id, [label:deviceName, name:deviceName])                 
+                state.temperatureZone = deviceConfig
+                updateTempratureSensor();
+            	break;
+        }	    
 	}
 }
 
+def String getRelayID(deviceConfig) {
+
+	return "piRelay." + settings.piIP + "." + deviceConfig
+}
+def String getTemperatureID(deviceConfig){
+    
+    return  "piTemp." + settings.piIP + "." + deviceConfig
+}
+
 def uninstalled() {
-	unsubscribe()
-	def delete = getChildDevices()
-	delete.each {
-		deleteChildDevice(it.deviceNetworkId)
-	}   
+  unsubscribe()
+  def delete = getChildDevices()
+    delete.each {
+    	unsubscribe(it)
+    	log.trace "about to delete device"
+        deleteChildDevice(it.deviceNetworkId)
+    }   
 }
 
 def response(evt){
-	log.debug "w00t"
-	log.debug evt
-	log.debug evt.value
+ def msg = parseLanMessage(evt.description);
+    if(msg && msg.body){
+    
+    	// This is the GPIO headder state message
+        def children = getChildDevices(false)
+    	if(msg.json) {
+           msg.json.GPIO.each { item ->
+                updateRelayDevice(item.key, item.value.value, children);
+            }
+            
+            log.trace "Finished Getting GPIO State"
+        }
+        
+        def tempContent = msg.body.tokenize('.')
+        if(tempContent.size() == 2 && tempContent[0].isNumber() && tempContent[1].isNumber() ) {
+            
+        	// Got temperature response            
+            def networkId = getTemperatureID(state.temperatureZone);
+            def theDevice = getChildDevices().find{ d -> d.deviceNetworkId.startsWith(networkId) }  
+            
+            if(theDevice) {
+                theDevice.setTemperature(msg.body, state.temperatureZone);
+                log.trace "$theDevice set to $msg.body"
+            }
+        }
+    }
+}
+
+def updateRelayDevice(GPIO, state, childDevices) {
+
+  	def theSwitch = childDevices.find{ d -> d.deviceNetworkId.endsWith(".$GPIO") }  
+    if(theSwitch) { 
+    	log.debug "Updating switch $theSwitch for GPIO $GPIO with value $state" 
+        theSwitch.changeSwitchState(state)
+    }
+}
+
+def updateTempratureSensor() {
+
+	log.trace "Updating temperature for $state.temperatureZone"
+	
+	executeRequest("/devices/" + state.temperatureZone  + "/sensor/temperature/c", "GET", false, null);
+    
+    runIn(60, updateTempratureSensor);
+}
+
+def updateGPIOState() { 
+
+	log.trace "Updating GPIO map"
+	
+	executeRequest("/*", "GET", false, null);
+    
+    runIn(10, updateGPIOState);
 }
 
 def switchChange(evt){
@@ -188,64 +318,47 @@ def switchChange(evt){
     setDeviceState(GPIO, state);
     
     return;
-    
-	// This code isnt really needed, but is useful to keep around for reference
-	def children = getChildDevices()
-	def theDeviceNetworkId = "piRelay." + deviceId + "." + GPIO;
-	log.debug theDeviceNetworkId;
-    
-	def theSwitch = children.find{ d -> d.deviceNetworkId == theDeviceNetworkId }  
-	log.debug "Got switch $theSwitch"
-    
-	switch(theSwitch){    
-	    case "on":
-		theSwitch.on();
-	    break;
-	    
-	case "off":
-		theSwitch.off();
-	    break;
-	}
-	
 }
+
 
 def setDeviceState(gpio, state) {
 	log.debug "Executing 'setDeviceState'"
-	
-	// Determine the path to post which will set the switch to the desired state
-	def Path = "/GPIO/" + gpio + "/value/";
+     
+    // Determine the path to post which will set the switch to the desired state
+    def Path = "/GPIO/" + gpio + "/value/";
 	Path += (state == "on") ? "1" : "0";
-	
-	executeRequest(Path, "POST", true, gpio);
+    
+    executeRequest(Path, "POST", true, gpio);
 }
 
 def executeRequest(Path, method, setGPIODirection, gpioPin) {
 		   
 	log.debug "The " + method + " path is: " + Path;
 	    
-	def headers = [:] 
-	headers.put("HOST", "$settings.piIP:$settings.piPort")
-	
-	try {    
-		if(setGPIODirection) {
-			def setDirection = new physicalgraph.device.HubAction(
-		    	method: "POST",
-		    	path: "/GPIO/" + gpioPin  + "/function/OUT",
-		    	headers: headers)
-		    
-			sendHubCommand(setDirection);
-		}
-		
-		def actualAction = new physicalgraph.device.HubAction(
-		    method: method,
-		    path: Path,
-		    headers: headers)
-		
-		sendHubCommand(actualAction)        
-	}
-	catch (Exception e) {
-		log.debug "Hit Exception $e on $hubAction"
-	}
+    def headers = [:] 
+    headers.put("HOST", "$settings.piIP:$settings.piPort")
+    
+    try {    	
+        
+        if(setGPIODirection) {
+        	def setDirection = new physicalgraph.device.HubAction(
+            	method: "POST",
+            	path: "/GPIO/" + gpioPin  + "/function/OUT",
+            	headers: headers)
+            
+        	sendHubCommand(setDirection);
+        }
+        
+        def actualAction = new physicalgraph.device.HubAction(
+            method: method,
+            path: Path,
+            headers: headers)
+        
+        sendHubCommand(actualAction)        
+    }
+    catch (Exception e) {
+        log.debug "Hit Exception $e on $hubAction"
+    }
 }
 
 /* Helper functions to get the network device ID */
